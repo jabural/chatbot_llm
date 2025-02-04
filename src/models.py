e22670a75f7541f63fdf55bf4bdfbf8e89d96dfb
jabural <jvr.burguete@gmail.com>
2025-02-04 23:23:26 +0100

    added auth

--- src/models.py
+++ src/models.py
@@ -1,8 +1,20 @@
 from .database import Base
-from sqlalchemy import Column, Integer, String, ForeignKey, DateTime, Text
+from sqlalchemy import Column, Integer, String, ForeignKey, DateTime, Text, Boolean
 from sqlalchemy.orm import relationship
 from datetime import datetime, UTC
 
+
+class Users(Base):
+    __tablename__ = 'users'
+
+    id = Column(Integer, primary_key=True, index=True)
+    email = Column(String, unique=True)
+    username = Column(String, unique=True)
+    first_name = Column(String)
+    last_name = Column(String)
+    hashed_password = Column(String)
+    is_active = Column(Boolean, default=True)
+    role = Column(String)
 
 class Thread(Base):
     __tablename__ = "threads"
